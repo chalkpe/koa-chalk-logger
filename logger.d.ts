119e5d0c0c4959785432fa5d7f2f60961fb4f8af
Chalk <chalkpe@gmail.com>
2018-02-26 17:55:55 +0900

    typescript support

---
+++ logger.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for koa-chalk-logger
+// Project: https://github.com/ChalkPE/koa-chalk-logger
+// Definitions by: Chalk <https://github.com/ChalkPE>
+
+import { Middleware } from 'koa'
+
+declare function KoaChalkLogger (): Middleware
+declare namespace KoaChalkLogger {}
+
+export = KoaChalkLogger
